699d0e7e459d3692eb04dd40650337908e43f808
echeadle <echeadle@gmail.com>
2024-03-24 10:22:16 -0600

    Finished day 5

---
+++ 05_day_configs/get_config_list.py
@@ -0,0 +1,18 @@
+import autogen
+
+"""
+    This is the basic way of handling Autogen configuration
+"""
+api_keys = ["sk-1231231"]
+base_urls = ["http://localhost:8001"]
+api_type = "openai"
+api_version = "api-preview-01"
+
+config_list = autogen.get_config_list(
+    api_keys,
+    base_urls,
+    api_type,
+    api_version
+)
+
+print(config_list)
